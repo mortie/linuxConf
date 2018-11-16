d8eb1c1377694add6c0ec2e5df5d1af478301a81
Martin Dørum <martid0311@gmail.com>
2018-11-16 07:50:23 +0100

    stuff

--- setup/zz-mort-custom.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-
