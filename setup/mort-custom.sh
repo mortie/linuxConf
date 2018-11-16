db1bf17641bf36542f7dae777bfa6def6c0e5e5b
Martin Dørum <martid0311@gmail.com>
2018-11-16 06:44:46 +0100

    replaced make function with make script

--- setup/mort-custom.sh
+++ setup/mort-custom.sh
@@ -1,3 +1,3 @@
-export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 
