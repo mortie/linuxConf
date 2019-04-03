926b2a8556daea5d41750901dba626d530e46bb4
Martin Dørum <martid0311@gmail.com>
2019-04-03 17:11:57 +0200

    rotation lock

--- .config/beanbar/beanbar.js
+++ .config/beanbar/beanbar.js
@@ -1,5 +1,48 @@
 css(THEME_DARK);
 
+class RotationLock extends ModComponent {
+	constructor() {
+		super();
+		this.setState({ locked: false });
+	}
+
+	componentDidMount() {
+		this.proc = new IPCProc(IPC_EXEC_SH, `
+			f=~/.rotation-lock
+			while read -r; do
+				if [ -f $f ]; then
+					rm -f $f
+					echo "unlocked"
+				else
+					touch $f
+					echo "locked"
+				fi
+			done
+			`, msg => this.setState({ locked: msg == "locked" }));
+		this.proc.send()
+	}
+
+	toggle() {
+		this.proc.send();
+	}
+
+	render(props, state) {
+		let className = "clickable ";
+		className += state.locked ? "locked " : "unlocked ";
+		return this.el({ className, onClick: this.toggle.bind(this) }, "Rotation");
+	}
+
+	css() {
+		return `
+		module.RotationLock.locked {
+			color: red;
+		}
+		module.RotationLock.unlocked {
+			color: lime;
+		}`;
+	}
+}
+
 css(`
 module.I3Workspaces .workspace.focused::after {
 	height: 2px;
@@ -18,6 +61,7 @@ init(
 		h(Drawer, null,
 			h(Launcher, { text: "Bluetooth", cmd: "blueman-manager" }),
 			h(Launcher, { text: "Audio", cmd: "pavucontrol" }),
+			h(RotationLock),
 		),
 	),
 );
